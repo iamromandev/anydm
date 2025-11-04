097cb5c492b37d4f3ccd787e20ca53f114e74225
Roman <iamromandev@gmail.com>
2025-11-04 13:37:38 +0600

    added vite-tsconfig-paths for resolving relative path using aliases @

--- apps/ui/src/entry.dev.tsx
+++ apps/ui/src/entry.dev.tsx
@@ -1,9 +1,7 @@
 import { render } from '@builder.io/qwik';
-import Root from './root';
+import Root from '@/root';
 
-const container: HTMLElement = document.getElementById('app');
+const container = document.getElementById('app');
 if (container) {
   render(container, <Root />);
-} else {
-  console.error('❌ #app container not found in HTML');
 }
--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [qwikVite({ csr: true })],
+  plugins: [qwikVite({ csr: true }), tsconfigPaths()],
 });
