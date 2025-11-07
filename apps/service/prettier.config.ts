f8788c83408fd13131eb76550684741bae270491
Roman <iamromandev@gmail.com>
2025-11-07 21:00:21 +0600

    hono hot mode

--- apps/service/prettier.config.ts
+++ apps/service/prettier.config.ts
@@ -5,10 +5,8 @@ export default {
     printWidth: 80,
     trailingComma: "all",
     plugins: [
-        "prettier-plugin-tailwindcss",
         "prettier-plugin-multiline-arrays",
     ],
-    tailwindConfig: "./tailwind.config.ts",
     multilineArraysWrapThreshold: 1,
     multilineArraysLinePattern: "1",
 } satisfies import("prettier").Config;
---
+++ apps/service/src/index.ts
@@ -0,0 +1,7 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+app.get("/", (c) => c.text("Hello Bun!"));
+
+export default app;
